a4cbf7fa89d8900c002b9dffc5d044aa4a73c968
zloyrock <zhzhonovsergey@gmail.com>
2016-01-28 15:16:06 +0300

    28.01.2016

---
+++ programm.php
@@ -0,0 +1,19 @@
+<?php
+	$name = $_POST[param1];
+	$phone = $_POST[param2];
+	$email = $_POST[param3];
+	$text = $_POST[param4];
+	$to = 'ya-mailfor2ch@yandex.ru';
+	$title = 'Сообщение от пользователя!';
+	$msgtext = "Имя: $name\nНомер телефона: $phone\nАдрес электронной почты: $email\nТекст сообщения: $text";
+
+	if (empty($name) or empty($phone) or empty($email) or empty($text)) {
+		echo "Заполните все поля!";
+	}
+	else {
+		if (mail($to,$title,$msgtext)) {
+			echo '1';
+		}
+		else echo '2';
+	};
+?>
